26e183c766f65d68b829fa8fcf14392265a8dfed
Rajesh <rajeshkumar.ramamurthy@intel.com>
2015-01-25 14:21:30 +0530

    Project Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -71,7 +71,7 @@ cacheSolve <- function(x, ...) {
 
 ## Retrieving from the cache in the second run
 ## > cacheSolve(m)
-## getting cached data.
+## getting cached data...
 ##           [,1]      [,2]
 ## [1,] -0.3333333  0.6666667
 ## [2,] 0.6666667 -0.3333333
